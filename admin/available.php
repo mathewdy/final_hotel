38a57da844e8afd3d11ba2ebbcc22c7662a8398e
Jade Mark <88232304+jd022@users.noreply.github.com>
2021-10-07 04:38:25 +0800

    initial commit

---
+++ admin/available.php
@@ -0,0 +1,67 @@
+<?php 
+include("../connection.php");
+session_start();
+  if(isset($_SESSION['username']) && isset($_SESSION['password'])){
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Home</title>
+</head>
+<body>
+   <a href="logout.php">Logout</a> 
+   <br>
+   <a href="home.php">Pending Request</a>
+   <a href="available.php">Available Rooms</a>
+   <a href="reserved.php">Reserved Rooms</a>
+   <a href="occupied.php">Occupied Rooms</a>
+
+   <?php 
+    $sql = "SELECT rooms.room_number, rooms.status, room_types.name_of_room FROM rooms
+    LEFT JOIN room_types ON rooms.room_type_id = room_types.id WHERE rooms.status = 'available' ORDER BY rooms.id ASC";
+    $query = mysqli_query($conn, $sql);
+    if(mysqli_num_rows($query) > 0){
+    ?>
+    <table>
+					<thead>
+					<tr>
+					<th>Rooms</th>
+					<th>Room Types</th>
+					<th>Status</th>
+					</tr>
+					</thead>
+				
+					<?php 
+						while($rows = mysqli_fetch_array($query)){
+					?>
+					<tbody>
+					<tr>
+					<td><?php echo $rows['room_number'];?></td>
+					<td><?php echo $rows['name_of_room'];?></td>
+					<td><?php echo $rows['status'];?></td>
+					</td>
+					</tr>
+					</tbody>
+					<?php
+					}
+					?>
+					</table>
+    
+    <?php
+    }
+    else{
+    ?>
+    <h3>No Available Rooms Yet</h3>
+    <?php
+    }
+   ?>
+</body>
+</html>
+<?php }else{
+  header("location:index.php");
+  exit();
+}
+?>
--- connection.php
+++ connection.php
@@ -1,5 +1,3 @@
 <?php
 $conn = new mysqli($_SERVER['HTTP_HOST'], "root" ,"" , "hotel");
-
-
 ?>
