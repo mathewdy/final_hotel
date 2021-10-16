5219cd51f8be6beb348a030fc7d449a6d43badee
mathewdy <mathewdalisay@gmail.com>
2021-10-17 01:35:03 +0800

    Update

--- back_end/logout.php
+++ back_end/logout.php
@@ -1,11 +1,11 @@
 <?php
 
 session_start();
-
+session_destroy();
 
 unset($_SESSION['email']);
 
-session_destroy();
+
 
 echo "<script>alert('log out') </script> ";
 echo "<script>window.location.href='../guest/index.php' </script>";
